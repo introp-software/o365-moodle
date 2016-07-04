d2dcf4aaebba882533f41969151b7f864bebe345
Nagesh <nagesh@introp.net>
2016-07-04 21:48:44 +0530

    Plugin version update

--- local/o365/db/upgrade.php
+++ local/o365/db/upgrade.php
@@ -481,14 +481,14 @@ function xmldb_local_o365_upgrade($oldversion) {
         upgrade_plugin_savepoint($result, 2015060131.01, 'local', 'o365');
     }
 
-    if ($result && $oldversion < 2015060132.01) {
+    if ($result && $oldversion < 2015111917.02) {
         if ($dbman->table_exists('local_o365_coursegroupdata')) {
             $table = new xmldb_table('local_o365_coursegroupdata');
             $field = new xmldb_field('classnotebook', XMLDB_TYPE_INTEGER, '1', null, null, null, '0', null);
             if (!$dbman->field_exists($table, $field)) {
                 $dbman->add_field($table, $field);
             }
-            upgrade_plugin_savepoint($result, '2015111917.01', 'local', 'o365');
+            upgrade_plugin_savepoint($result, '2015060132.02', 'local', 'o365');
         }
     }
 
