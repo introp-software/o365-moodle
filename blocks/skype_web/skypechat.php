fe104844d7f8588c4429573701b77f132da6b27f
Aashay <aashay@introp.net>
2016-11-25 18:44:25 +0530

    Added localization in Skype chat and resolved context error

--- blocks/skype_web/skypechat.php
+++ blocks/skype_web/skypechat.php
@@ -1,22 +1,29 @@
 <?php
 
-    require_once(__DIR__ . '/../../config.php');
-    $client_id = get_config('auth_oidc', 'clientid');
-    $config = array('client_id' => $client_id, 'wwwroot' => $CFG->wwwroot);
+require_once(__DIR__ . '/../../config.php');
+require_login();
+$client_id = get_config('auth_oidc', 'clientid');
+$config = array('client_id' => $client_id, 'wwwroot' => $CFG->wwwroot);
 
-    global $PAGE;
+global $PAGE;
 
-    $PAGE->set_url('/blocks/skype_web/skypechat.php');
-    $PAGE->set_title('skype chat');
-    $skypesdkurl = new moodle_url('https://swx.cdn.skype.com/shared/v/1.2.9/SkypeBootstrap.js');
-    $PAGE->requires->js($skypesdkurl, true);
-    $PAGE->requires->jquery();
-    $PAGE->requires->jquery_plugin('ui');
-    $PAGE->requires->jquery_plugin('ui-css');
-    $PAGE->requires->yui_module('moodle-block_skype_web-signin', 'M.block_skype_web.signin.init', array($config));
-    $PAGE->requires->yui_module('moodle-block_skype_web-chatservice', 'M.block_skype_web.chatservice.init', array($config));
-    
-    echo $OUTPUT->header();
-    echo file_get_contents($CFG->dirroot.'/blocks/skype_web/html_templates/skype_chat.html');
-    echo $OUTPUT->footer();
+$PAGE->set_context(context_system::instance());
+$PAGE->set_url('/blocks/skype_web/skypechat.php');
+$PAGE->set_title('skype chat');
+$skypesdkurl = new moodle_url('https://swx.cdn.skype.com/shared/v/1.2.9/SkypeBootstrap.js');
+$PAGE->requires->js($skypesdkurl, true);
+$PAGE->requires->jquery();
+$PAGE->requires->jquery_plugin('ui');
+$PAGE->requires->jquery_plugin('ui-css');
+$PAGE->requires->yui_module('moodle-block_skype_web-signin', 'M.block_skype_web.signin.init', array($config));
+$PAGE->requires->yui_module('moodle-block_skype_web-chatservice', 'M.block_skype_web.chatservice.init', array($config));
+
+echo $OUTPUT->header();
+$templateengine = new Mustache_Engine();
+$bodycontents = $templateengine->render(file_get_contents($CFG->dirroot . '/blocks/skype_web/html_templates/skype_chat.html'), array('get_string' => function($stringtolocalize) {
+        return get_string($stringtolocalize, 'block_skype_web');
+    }));
+
+echo $bodycontents;
+echo $OUTPUT->footer();
 ?>
